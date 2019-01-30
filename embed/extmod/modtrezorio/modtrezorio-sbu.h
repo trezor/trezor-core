d427fc337363570237fdab94b44fa0e49fad26c8
Pavol Rusnak <pavol@rusnak.io>
2019-01-30 17:39:08 +0100

    embed/extmod/modtrezorio: add more comments to SBU functions

--- embed/extmod/modtrezorio/modtrezorio-sbu.h
+++ embed/extmod/modtrezorio/modtrezorio-sbu.h
@@ -19,6 +19,9 @@
 
 #include "sbu.h"
 
+#define SBU_DEFAULT_READ_TIMEOUT 10000
+#define SBU_DEFAULT_WRITE_TIMEOUT 10000
+
 /// class SBU:
 ///     '''
 ///     '''
@@ -28,6 +31,7 @@ typedef struct _mp_obj_SBU_t {
 
 /// def __init__(self) -> None:
 ///     '''
+///     Initialize SBU in GPIO mode (UART disabled)
 ///     '''
 STATIC mp_obj_t mod_trezorio_SBU_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args) {
     mp_arg_check_num(n_args, n_kw, 0, 0, false);
@@ -39,12 +43,12 @@ STATIC mp_obj_t mod_trezorio_SBU_make_new(const mp_obj_type_t *type, size_t n_ar
 
 /// def read(buffer: bytearray, timeout=10000) -> int:
 ///     '''
-///     Reads from SBU
+///     Reads from SBU UART (timeout is in milliseconds)
 ///     '''
 STATIC mp_obj_t mod_trezorio_SBU_read(size_t n_args, const mp_obj_t *args) {
     mp_buffer_info_t b;
     mp_get_buffer_raise(args[1], &b, MP_BUFFER_WRITE);
-    uint32_t timeout = n_args > 2 ? mp_obj_get_int(args[2]) : 10000;
+    uint32_t timeout = n_args > 2 ? mp_obj_get_int(args[2]) : SBU_DEFAULT_READ_TIMEOUT;
     int res = sbu_read(b.buf, b.len, timeout);
     return MP_OBJ_NEW_SMALL_INT(res);
 }
@@ -52,12 +56,12 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(mod_trezorio_SBU_read_obj, 2, 3, mod_
 
 /// def write(buffer: bytearray, timeout=10000) -> None:
 ///     '''
-///     Writes to SBU
+///     Writes to SBU UART (timeout is in milliseconds)
 ///     '''
 STATIC mp_obj_t mod_trezorio_SBU_write(size_t n_args, const mp_obj_t *args) {
     mp_buffer_info_t b;
     mp_get_buffer_raise(args[1], &b, MP_BUFFER_READ);
-    uint32_t timeout = n_args > 2 ? mp_obj_get_int(args[2]) : 10000;
+    uint32_t timeout = n_args > 2 ? mp_obj_get_int(args[2]) : SBU_DEFAULT_WRITE_TIMEOUT;
     sbu_write(b.buf, b.len, timeout);
     return mp_const_none;
 }
@@ -65,7 +69,7 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(mod_trezorio_SBU_write_obj, 2, 3, mod
 
 /// def set_uart(self, serial: bool) -> None:
 ///     '''
-///     Sets SBU wires to sbu1 and sbu2 values respectively
+///     Enables/disables SBU UART (disabled = SBU pins are GPIO)
 ///     '''
 STATIC mp_obj_t mod_trezorio_SBU_set_uart(mp_obj_t self, mp_obj_t serial) {
     if (mp_obj_is_true(serial)) {
@@ -79,7 +83,7 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_2(mod_trezorio_SBU_set_uart_obj, mod_trezorio_SBU
 
 /// def set_pins(self, sbu1: bool, sbu2: bool) -> None:
 ///     '''
-///     Sets SBU wires to sbu1 and sbu2 values respectively
+///     Set GPIO values for SBU1 and SBU2 pins (if UART is disabled)
 ///     '''
 STATIC mp_obj_t mod_trezorio_SBU_set_pins(mp_obj_t self, mp_obj_t sbu1, mp_obj_t sbu2) {
     sbu_set_pins(sectrue * mp_obj_is_true(sbu1), sectrue * mp_obj_is_true(sbu2));
