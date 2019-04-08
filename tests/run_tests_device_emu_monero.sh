20146fdcca0dd81f6e5d68c4f6e2dcdef3fcc8aa
Dusan Klinec <dusan.klinec@gmail.com>
2019-04-08 13:39:53 +0200

    tests: monero binary test added

--- tests/run_tests_device_emu_monero.sh
+++ tests/run_tests_device_emu_monero.sh
@@ -10,6 +10,18 @@ upy_pid=$!
 sleep 1
 
 export TREZOR_PATH=udp:127.0.0.1:21324
+DOCKER_ID=""
+
+# Test termination trap
+terminate_test() {
+  if [[ $# > 0 ]]; then error=$1; fi
+  kill $upy_pid
+  if [ -n "$DOCKER_ID" ]; then docker kill $DOCKER_ID 2>/dev/null >/dev/null; fi 
+  exit $error
+}
+
+set -e
+trap 'terminate_test $?' EXIT
 
 # run tests
 cd ..
@@ -18,10 +30,62 @@ export EC_BACKEND_FORCE=1
 export EC_BACKEND=1
 export TREZOR_TEST_GET_TX=1
 export TREZOR_TEST_LIVE_REFRESH=1
-export TREZOR_TEST_SIGN_CL0_HF9=1
+export TREZOR_TEST_SIGN_CL0_HF9=0  # HF9 is no longer active
 export TREZOR_TEST_SIGN_CL1_HF9=1
 export TREZOR_TEST_SIGN_CL1_HF10=1
+
 python3 -m unittest trezor_monero_test.test_trezor
 error=$?
-kill $upy_pid
+test $error != 0 && exit $error
+
+if [[ "$OSTYPE" != "linux-gnu" && "$OSTYPE" != "darwin"* ]]; then
+  echo "Tests with native Monero app is supported only on Linux and OSX at the moment. Your OS: $OSTYPE"
+  exit 0
+fi
+
+error=1
+TREZOR_MONERO_TESTS_URL=${TREZOR_MONERO_TESTS_URL:-https://github.com/ph4r05/monero/releases/download/v0.14.0.2-tests-u14.04/trezor_tests}
+TREZOR_MONERO_TESTS_SHA256SUM=${TREZOR_MONERO_TESTS_SHA256SUM:-540c2f9b324b538a32254bde5377dee86b0b8d3ae5b29da8005bcfa274e2ae5c}
+TREZOR_MONERO_TESTS_PATH=${TREZOR_MONERO_TESTS_PATH:-./trezor_monero_tests}
+TREZOR_MONERO_TESTS_LOG=${TREZOR_MONERO_TESTS_LOG:-./trezor_monero_tests.log}
+
+if [[ ! -f $TREZOR_MONERO_TESTS_PATH || "`shasum -a256 $TREZOR_MONERO_TESTS_PATH | cut -d' ' -f1`" != $TREZOR_MONERO_TESTS_SHA256SUM ]]; then 
+  echo "Downloading Trezor monero tests binary `pwd` $TREZOR_MONERO_TESTS_PATH"
+  curl -s -L -o $TREZOR_MONERO_TESTS_PATH $TREZOR_MONERO_TESTS_URL \
+    && chmod +x $TREZOR_MONERO_TESTS_PATH \
+    && test "`shasum -a256 $TREZOR_MONERO_TESTS_PATH | cut -d' ' -f1`" == "$TREZOR_MONERO_TESTS_SHA256SUM" || exit 1
+fi
+
+TIME_TESTS_START=$SECONDS
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  $TREZOR_MONERO_TESTS_PATH 2>&1 > "$TREZOR_MONERO_TESTS_LOG"
+  error=$?
+
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  CORE_DIR=`pwd` 
+  DOCKER_ID=$(docker run -idt --mount type=bind,src=$CORE_DIR,dst=$CORE_DIR -w $CORE_DIR --network=host ubuntu:18.04)    
+  docker exec $DOCKER_ID apt-get update -qq
+  docker exec $DOCKER_ID apt-get install --no-install-recommends --no-upgrade -qq net-tools socat
+  docker exec -d $DOCKER_ID socat UDP-LISTEN:21324,reuseaddr,reuseport,fork UDP4-SENDTO:host.docker.internal:21324
+  docker exec -d $DOCKER_ID socat UDP-LISTEN:21325,reuseaddr,reuseport,fork UDP4-SENDTO:host.docker.internal:21325
+  docker exec $DOCKER_ID $TREZOR_MONERO_TESTS_PATH 2>&1 > "$TREZOR_MONERO_TESTS_LOG"
+  error=$? 
+ 
+else 
+  echo "Unsupported OS: $OSTYPE"
+  exit 1
+fi
+
+
+TIME_TESTS_ELAPSED=$((SECONDS-TIME_TESTS_START))
+
+if ((error != 0)); then
+  echo "ERROR in trezor tests. Log follows;"
+  tail -n 500 "$TREZOR_MONERO_TESTS_LOG"
+else
+  echo "[PASS] Monero test in $TIME_TESTS_ELAPSED sec. "
+  cat "$TREZOR_MONERO_TESTS_LOG" | grep -v DEBUG | egrep '#TEST#|tests.core\b' | tail -n 50
+fi
+
 exit $error
+
